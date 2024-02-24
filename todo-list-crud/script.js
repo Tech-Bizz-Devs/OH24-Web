2d2680a4812d9b555ee7e9f7635e5513714748e0
Jatin <75193966+oyejateen@users.noreply.github.com>
2024-02-24 02:14:27 -0800

    Update script.js

--- todo-list-crud/script.js
+++ todo-list-crud/script.js
@@ -14,38 +14,44 @@ filters.forEach((btn) => {
     showToDo(btn.id);
   });
 });
-
-function showToDo(filter) {
-  let liTag = "";
-  if (todos) {
-    todos.forEach((todo, id) => {
-      let completed = todo.status == "completed" ? "checked" : "";
-      if (filter == todo.status || filter == "all") {
-        liTag += `<li class="task">
-                      <label for="${id}">
-                          <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
-                          <p class="${completed}">${todo.name}</p>
-                      </label>
-                      <div class="settings">
-                          <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
-                          <ul class="task-menu">
-                              <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
-                              <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
-                          </ul>
-                      </div>
-                  </li>`;
-      }
-    });
+function updateURL(filter) {
+    window.history.pushState({}, '', `/API/${filter}`);
   }
-  taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
-  let checkTask = taskBox.querySelectorAll(".task");
-  !checkTask.length
-    ? clearAll.classList.remove("active")
-    : clearAll.classList.add("active");
-  taskBox.offsetHeight >= 300
-    ? taskBox.classList.add("overflow")
-    : taskBox.classList.remove("overflow");
-}
+
+  function showToDo(filter) {
+    let liTag = "";
+    if (todos) {
+      todos.forEach((todo, id) => {
+        let completed = todo.status == "completed" ? "checked" : "";
+        if (filter == todo.status || filter == "all") {
+          liTag += `<li class="task">
+                        <label for="${id}">
+                            <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
+                            <p class="${completed}">${todo.name}</p>
+                        </label>
+                        <div class="settings">
+                            <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                            <ul class="task-menu">
+                                <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
+                                <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
+                            </ul>
+                        </div>
+                    </li>`;
+        }
+      });
+    }
+    taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
+    let checkTask = taskBox.querySelectorAll(".task");
+    !checkTask.length
+      ? clearAll.classList.remove("active")
+      : clearAll.classList.add("active");
+    taskBox.offsetHeight >= 300
+      ? taskBox.classList.add("overflow")
+      : taskBox.classList.remove("overflow");
+    // Call updateURL outside of the loop
+    updateURL(filter);
+  }
+  
 
 showToDo("all");
 
@@ -77,6 +83,7 @@ function editTask(taskId, textName) {
   taskInput.value = textName;
   taskInput.focus();
   taskInput.classList.add("active");
+  updateURL('edit');
 }
 
 function deleteTask(deleteId, filter) {
@@ -84,13 +91,16 @@ function deleteTask(deleteId, filter) {
   todos.splice(deleteId, 1);
   localStorage.setItem("todo-list", JSON.stringify(todos));
   showToDo(filter);
+  updateURL('delete');
+
 }
 
 clearAll.addEventListener("click", () => {
   isEditTask = false;
   todos.splice(0, todos.length);
   localStorage.setItem("todo-list", JSON.stringify(todos));
-  showToDo();
+  showToDo("all");
+  updateURL('delete');
 });
 
 taskInput.addEventListener("keyup", (e) => {
@@ -107,5 +117,6 @@ taskInput.addEventListener("keyup", (e) => {
     taskInput.value = "";
     localStorage.setItem("todo-list", JSON.stringify(todos));
     showToDo(document.querySelector("span.active").id);
+    updateURL('add');
   }
 });
