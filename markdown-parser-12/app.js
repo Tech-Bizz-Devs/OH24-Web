b12ff8f562313a0c179fff2cb72419159d88a1e1
Rituraj Jha <riturajreal01@gmail.com>
2024-02-25 12:23:18 +0530

    rm files

--- markdown-parser-12/app.js
+++
@@ -1,9 +0,0 @@
-import MarkdownIt from 'markdown-it';
-
-const md = new MarkdownIt();
-
-function convertMarkdown() {
-  const markdownInput = document.getElementById('markdownInput').value;
-  const htmlOutput = md.render(markdownInput);
-  document.getElementById('htmlOutput').innerHTML = htmlOutput;
-}
