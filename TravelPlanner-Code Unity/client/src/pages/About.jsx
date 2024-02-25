374259f91551da2254062171424de01a3469cfac
Pranjal Jain <jainpranjal30112002@gmail.com>
2024-02-25 16:09:17 +0530

    Travel Planner Project

---
+++ TravelPlanner-Code Unity/client/src/pages/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Travel Unity
+      </h1>
+      <p className="mb-4 text-slate-700">
+        The Travel Unity's aim is for helping users in planning and organizing
+        trips efficiently.{" "}
+      </p>
+      <p className="mb-4 text-slate-700">
+        This platform empower users to seamlessly navigate through various
+        aspects of trip planning, including destination search, accommodation
+        and transportation options, itinerary creation, expense budgeting, and
+        social sharing capabilities.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our team of agents has a wealth of experience and knowledge in the
+        travelling industry, and we are committed to providing the highest level
+        of service to our clients. We believe that travelling different places 
+        should be an exciting and rewarding experience, and we are dedicated to
+        making that a reality for each and every one of our clients.
+      </p>
+    </div>
+  );
+}
